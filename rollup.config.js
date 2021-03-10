af7b04f9a9c80a030ee65669c803d90b38ab8e53
Nagamallesh V <49677363+nagamalleshavenkategowda@users.noreply.github.com>
2021-03-10 14:00:07 +0530

    feature add esm builds

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,11 @@ import postcss from 'rollup-plugin-postcss';
 import url from "@rollup/plugin-url";
 const cssUrl = require("postcss-url")
 
-export default {
+export default  [{
   input: 'src/index.js',
   output: [
     {
-      file: './dist/easy-drawing-board.js',
+      file: './dist/easy-drawing-board.umd.js',
       format: 'umd',
       name: 'EasyDrawingBoard'
     }
@@ -24,4 +24,40 @@ export default {
     resolve(),
     babel({ babelHelpers: 'bundled' })
   ]
-}
+}, {
+  input: 'src/index.js',
+  output: [
+    {
+      file: './dist/easy-drawing-board.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    terser(),
+    postcss({
+      modules: false,
+      plugins: [cssUrl({url: 'inline'})]
+    }),
+    url(),
+    resolve(),
+    babel({ babelHelpers: 'bundled' })
+  ]
+}, {
+  input: 'src/index.js',
+  output: [
+    {
+      file: './dist/easy-drawing-board.iife.js',
+      format: 'iife'
+    }
+  ],
+  plugins: [
+    terser(),
+    postcss({
+      modules: false,
+      plugins: [cssUrl({url: 'inline'})]
+    }),
+    url(),
+    resolve(),
+    babel({ babelHelpers: 'bundled' })
+  ]
+}]
